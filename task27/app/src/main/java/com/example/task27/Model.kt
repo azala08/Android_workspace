e31a5ea633cc7835f0f838c0d49164a2c7bce6c2
Aditi Zala <aditi.zala08@gmail.com>
2025-03-03 15:26:20 +0530

    commit

---
+++ task27/app/src/main/java/com/example/task27/Model.kt
@@ -0,0 +1,16 @@
+package com.example.task27
+
+import retrofit2.http.Field
+
+class Model
+{
+    var name:String=""
+    var surname:String=""
+    var email:String=""
+    var gender:String=""
+    var mobile:String=""
+    var password:String=""
+
+
+
+}
